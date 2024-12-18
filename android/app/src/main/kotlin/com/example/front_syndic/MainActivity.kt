187f1a2adead1e85a93559207c787b3e53cc544f
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-12-18 23:15:28 +0100

    Stripe part

--- android/app/src/main/kotlin/com/example/front_syndic/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/front_syndic/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.front_syndic
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
