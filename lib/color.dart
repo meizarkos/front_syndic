d42305e04ccf3b5dc9b1d785986d78bd56de1c05
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-04-21 17:08:59 +0200

    Change of color

--- lib/color.dart
+++ lib/color.dart
@@ -5,6 +5,6 @@ class AppColors{
   static const Color mainBackgroundColor = Color(0xFFFBF4CD);
   static const Color rowBackgroundColor = Color(0xFFFFEA09);
   static const Color mainErrorColor = Colors.red;
-  static const Color mainTextColor = Color(0xFF62A170);
+  static const Color mainTextColor = Color(0xFF4E8159);
   static const Color actionButtonColor = Color(0xFFFF5454);
 }
